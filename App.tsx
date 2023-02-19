c917bc852f7c8abec6a3553feec8ac9046bac38b
allancassio <allan.cassio1@gmail.com>
2023-02-19 20:23:36 -0300

    create app I am Here

--- App.tsx
+++ App.tsx
@@ -1,25 +1,11 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StatusBar, View } from "react-native";
 import { Home } from "./src/screens/Home";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#131016",
-    padding: 24,
-  },
-  eventName: {
-    color: "#fdfcfe",
-    marginTop: 48,
-    fontWeight: "bold",
-    fontSize: 24,
-  },
-  eventDate: {
-    color: "#6b6b6b",
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-});
-
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" translucent />
+      <Home />
+    </>
+  );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,85 @@
-import { Text, TextInput, View } from "react-native";
+import {
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Alert,
+} from "react-native";
 import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
+import { useState } from "react";
 
 export function Home() {
+  const [participants, setParticipants] = useState<string[]>(["joão"]);
+
+  const handleParticipantAdd = () => {
+    // if (participants.includes(name)) {
+    //   alert(`já exite ${"item"}`);
+    // }
+    setParticipants((prevState) => [...prevState, "ana"]);
+  };
+  const handleParticipantRemove = (name: string) => {
+    Alert.alert("Remover", `remove ${name}`, [
+      {
+        text: "sim",
+        onPress() {
+          Alert.alert("deletado");
+        },
+      },
+      {
+        text: "não",
+        style: "cancel",
+      },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 29 de fevereiro de 2023</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6b6b6b"
-        keyboardType="visible-password"
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante."
+          placeholderTextColor="#6b6b6b"
+          keyboardType="visible-password"
+        />
+        {/* TouchableOpacity -> butão clicavel  */}
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={participants}
+        keyExtractor={(item, i) => String(i)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item, index }) => (
+          <Participant
+            key={index}
+            name={item}
+            onRemove={handleParticipantRemove}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Niguém chegou no evento ainda? Adicione participantes a sua lista de
+            presença.
+          </Text>
+        )}
       />
+      {/* ScrollView -> rolagem */}
+      {/* showsVerticalScrollIndicator -> barra de rolagem desativada */}
+      {/* <ScrollView showsVerticalScrollIndicator={false}>
+        {participants.map((participant, index) => (
+          <Participant
+            name={participant}
+            onRemove={handleParticipantRemove}
+            key={index}
+          />
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
